c4868803547c9b798c28b7c078412d260bb6d0c7
Mariam Fatima <118051988+mariamffatima@users.noreply.github.com>
2023-07-15 18:30:34 +0500

    responsiveness added
    this updated version is responsive to multiple size screens

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,16 @@
-import { Inter } from 'next/font/google'
+import { Inter } from 'next/font/google';
 import Image from 'next/image';
 import { FaSearch } from 'react-icons/fa';
 import {FiGlobe} from 'react-icons/fi';
 import {BsList} from 'react-icons/bs';
 import {FaUserCircle} from 'react-icons/fa';
 import React, { useState } from 'react';
-import {BsFillArrowLeftCircleFill} from 'react-icons/bs'
-import {BsFillArrowRightCircleFill} from 'react-icons/bs'
-
+import {BsFillArrowLeftCircleFill} from 'react-icons/bs';
+import {BsFillArrowRightCircleFill} from 'react-icons/bs';
+import 'react-date-range/dist/styles.css'; // main css file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { DateRange } from 'react-date-range';
+import { DefinedRange } from 'react-date-range';
 const inter = Inter({ subsets: ['latin'] })
 
 function MyInput() {
@@ -22,9 +25,10 @@ function MyInput() {
           onChange={handleInputChange}
           className='input-box pl-5 pr-1 flex-grow bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400'
           placeholder='Start your search'/>
-      <p className=' flex justify-between h-8 w-8 bg-red-400 rounded-full p-2 mr-2'>
-        <FaSearch className='cursor-pointer text-white' />
-      </p>
+          <div className='hidden md:inline'>
+      <p className=' flex overflow-hidden  justify-between h-8 w-8 bg-red-400 rounded-full p-2 mr-2'>
+        <FaSearch className='cursor-pointer overflow-hidden text-white' />
+      </p></div>
     </div>
   );
 }
@@ -33,9 +37,7 @@ export default function Home() {
   return (
     <body>
       <title>airbnb</title> 
-
         <header className=' top-0 bottom-0 z-50 grid grid-cols-3 bg-shadow-none px-5 bg-white p-5 md:px-10'>
-
         <div className='shadow-none top-0 z-50 grid-cols-3 bg-white  pr-5 pl-0'>
           <div className='relative flex items-center h-10 cursor-pointer my-auto'>
             <div className=' origin-top-left'>
@@ -44,32 +46,36 @@ export default function Home() {
         </div>
         </div>
 
-        <div className=' md:border-2 rounded-full py-2 md:shadow-sm hover:shadow-lg duration-300 mr-15'>
-          <MyInput />
+        <div className=' md:border-2 rounded-full py-2 md:lg:shadow-sm hover:shadow-lg duration-300 mr-15'>
+          <MyInput/>
+         
         </div>
         
         <div className='flex items-center space-x-3 justify-end text-gray-600'>
           <div className='hover:bg-gray-100 flex flex-row rounded-full p-3'>
             <p className='hidden md:inline cursor-text'>Become a host</p>
             </div>
-
-            <div className='hover:bg-gray-100  p-3 rounded-full'>
-              
+            <div className='hover:bg-gray-100 p-3 rounded-full'>
+              <p className='hidden md:inline'>
               <FiGlobe className='text-lg flex items-center cursor-pointer h-6 fill-none mr-3' />
+              </p>
             </div>
             <div className='flex items-center text-lg mr-3 space-x-3 border-2 p-3 rounded-full hover:shadow-lg duration-300'>
               <BsList className='cursor-auto h-6 fill-current'/>
                 <FaUserCircle className='h-6 cursor-auto fill-current'/>
               </div>
             </div>
+           <div className='flex flex-row justify-center align-middle'>
+            <DefinedRange/><DateRange />
+            </div>
       </header>
 
       <div className='relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px] '>
         <span className='overflow-hidden bg-none opacity-1 border-0 m-0 p-0 inset-0'>
-        <Image src="/home.jpg" alt="My Image" width={1800} height={50} />
+        <Image src="/home.jpg" alt="My Image" layout='fill' objectFit='cover'/>
         </span>
         <div className=' absolute top-1/2 w-full text-center'>
-          <p className='font-bold text-sm sm:text-xl pb-10 '>
+          <p className='font-bold text-sm md:text-xl pb-3 '>
             Not sure where to go? Perfect.
           </p>
           <button className='text-purple-500 bg-white px-10 py-4 shadow-md rounded-full font-bold my-3 hover:shadow-xl active:scale-90 transition duration-150'>
@@ -87,7 +93,7 @@ export default function Home() {
           <div className='flex items-center m-2 mt-5 space-x-4 rounded-xl cursor-pointer hover:bg-gray-100 hover:scale-105 transition transform duration-200 ease-out'>
               <div className='relative h-16 w-16'>
                 <span className=''>
-                <Image src="/i1.jpg" alt="My Image" width={1800} height={2000} />
+                <Image src="/i1.jpg" alt="My Image" layout='fill' objectFit='cover' />
                 </span>
               </div>
               <div >
@@ -102,7 +108,7 @@ export default function Home() {
             <div className='flex items-center m-2 mt-5 space-x-4 rounded-xl cursor-pointer hover:bg-gray-100 hover:scale-105 transition transform duration-200 ease-out'>
               <div className='relative h-16 w-16'>
                 <span className=''>
-                <Image src="/i2.jpg" alt="My Image" width={1800} height={2000} />
+                <Image src="/i2.jpg" alt="My Image" layout='fill' objectFit='cover' />
                 </span>
               </div>
               <div >
@@ -117,7 +123,7 @@ export default function Home() {
             <div className='flex items-center m-2 mt-5 space-x-4 rounded-xl cursor-pointer hover:bg-gray-100 hover:scale-105 transition transform duration-200 ease-out'>
               <div className='relative h-16 w-16'>
                 <span className=''>
-                <Image src="/i3.jpg" alt="My Image" width={1800} height={2000} />
+                <Image src="/i3.jpg" alt="My Image" layout='fill' objectFit='cover' />
                 </span>
               </div>
               <div >
@@ -132,7 +138,7 @@ export default function Home() {
             <div className='flex items-center m-2 mt-5 space-x-4 rounded-xl cursor-pointer hover:bg-gray-100 hover:scale-105 transition transform duration-200 ease-out'>
               <div className='relative h-16 w-16'>
                 <span className=''>
-                <Image src="/i4.jpg" alt="My Image" width={1800} height={2000} />
+                <Image src="/i4.jpg" alt="My Image" layout='fill' objectFit='cover' />
                 </span>
               </div>
               <div >
@@ -147,7 +153,7 @@ export default function Home() {
             <div className='flex items-center m-2 mt-5 space-x-4 rounded-xl cursor-pointer hover:bg-gray-100 hover:scale-105 transition transform duration-200 ease-out'>
               <div className='relative h-16 w-16'>
                 <span className=''>
-                <Image src="/i5.jpg" alt="My Image" width={1800} height={2000} />
+                <Image src="/i5.jpg" alt="My Image" layout='fill' objectFit='cover' />
                 </span>
               </div>
               <div >
@@ -162,7 +168,7 @@ export default function Home() {
             <div className='flex items-center m-2 mt-5 space-x-4 rounded-xl cursor-pointer hover:bg-gray-100 hover:scale-105 transition transform duration-200 ease-out'>
               <div className='relative h-16 w-16'>
                 <span className=''>
-                <Image src="/i6.jpg" alt="My Image" width={1800} height={2000} />
+                <Image src="/i6.jpg" alt="My Image" layout='fill' objectFit='cover' />
                 </span>
               </div>
               <div >
@@ -177,7 +183,7 @@ export default function Home() {
             <div className='flex items-center m-2 mt-5 space-x-4 rounded-xl cursor-pointer hover:bg-gray-100 hover:scale-105 transition transform duration-200 ease-out'>
               <div className='relative h-16 w-16'>
                 <span className=''>
-                <Image src="/i7.jpg" alt="My Image" width={1800} height={2000} />
+                <Image src="/i7.jpg" alt="My Image" layout='fill' objectFit='cover' />
                 </span>
               </div>
               <div >
@@ -192,7 +198,7 @@ export default function Home() {
             <div className='flex items-center m-2 mt-5 space-x-4 rounded-xl cursor-pointer hover:bg-gray-100 hover:scale-105 transition transform duration-200 ease-out'>
               <div className='relative h-16 w-16'>
                 <span className=''>
-                <Image src="/i8.jpg" alt="My Image" width={1800} height={2000} />
+                <Image src="/i8.jpg" alt="My Image" layout='fill' objectFit='cover' />
                 </span>
               </div>
               <div >
@@ -228,8 +234,7 @@ export default function Home() {
            <div className='cursor--pointer hover:scale-105 transform transition duration-300'>
               <div className='relative h-80 w-80'>
                 <span className='box-border block  bg-none opacity-1 border-0 inset-0 scroll-smooth ' >
-                <Image src="/j1.jpg" alt="My Image" className=' rounded-lg border-none inset-0 block'
-                 width={300} height={320} />
+                <Image src="/j1.jpg" alt="My Image" className=' rounded-lg border-none inset-0 block' layout='fill' objectFit='fit' />
                 </span>
               </div>
               <h3 className='text-2xl m-0'>Outdoor getaways</h3>
@@ -239,7 +244,7 @@ export default function Home() {
               <div className='relative h-80 w-80'>
                 <span className='box-border block  bg-none opacity-1 border-0 inset-0 scroll-smooth ' >
                 <Image src="/j2.jpg" alt="My Image" className=' rounded-lg border-none inset-0 block'
-                 width={300} height={320} />
+                 layout='fill' objectFit='cover' />
                 </span>
               </div>
               <h3 className='text-2xl m-0'>Unique stays</h3>
@@ -249,7 +254,7 @@ export default function Home() {
                 <div className='relative h-80 w-80'>
                   <span className='box-border block  bg-none opacity-1 border-0 inset-0 scroll-smooth ' >
                       <Image src="/j3.jpg" alt="My Image" className=' rounded-lg border-none inset-0 block'
-                       width={300} height={320} />
+                       layout='fill' objectFit='cover' />
                   </span>
                 </div>
                   <h3 className='text-2xl m-0'>Entire homes</h3>
@@ -259,7 +264,7 @@ export default function Home() {
                 <div className='relative h-80 w-80'>
                   <span className='box-border block  bg-none opacity-1 border-0 inset-0 scroll-smooth ' >
                       <Image src="/j4.jpg" alt="My Image" className=' rounded-lg border-none inset-0 block'
-                       width={300} height={320} />
+                       layout='fill' objectFit='cover' />
                   </span>
                 </div>
                   <h3 className='text-2xl m-0'>Pet allowed</h3>
@@ -272,17 +277,19 @@ export default function Home() {
         <section className='realtive py-16 cursor-pointer'>
           <div className='relative h-96 min-w-[300px]'>
             <span className='bg-none box-border block overflow-hidden opacity-1 border-0 m-0 p-0 absolute inset-0'>
-            <Image src="/bbanner.jpg" alt="My Image" width={1000} height={1000} className='rounded-2xl border-none m-auto inset-0 box-border p-0' />
+            <Image src="/bbanner.jpg" alt="My Image" layout='fill' objectFit='cover'  className='rounded-2xl border-none m-auto inset-0 box-border p-0' />
             </span>
          
-          <div className =' absolute top-32 px-12 ml-12'>
+          <div className =' absolute flex-col justify-center  top-12 px-12 '>
+
             <h3 className='text-4xl mb-3 w-64'>
               The Greatest Outdoors
             </h3>
-            <p >Whishlists curated by Airbnb</p>
+            <h5 >Whishlists curated by Airbnb</h5>
             <button className='text-sm text-white bg-gray-900 px-4 py-2 rounded-lg mt-5 hover:scale-105 transition transform duration-200 ease-out'>
               Get Inspired
             </button>
+
           </div>
           </div>
         </section>
@@ -317,10 +324,10 @@ export default function Home() {
           </h5>
           <ul className='space-y-4'>
           <li>
-              Try hAsting
+              Try hosting
             </li>
             <li>
-              AirCover: protection forAHosts
+              AirCover: protection for Hosts
             </li>
             <li>
               Explore hostion resAurces
